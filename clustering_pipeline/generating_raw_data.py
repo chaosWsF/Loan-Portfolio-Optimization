6a455c955a7d8e956dec72804e4d54e6e555ae9a
wswordform <25746440+chaosWsF@users.noreply.github.com>
2022-03-07 11:52:01 -0700

    add logging

--- clustering_pipeline/generating_raw_data.py
+++ clustering_pipeline/generating_raw_data.py
@@ -1,40 +1,60 @@
+import logging
 import pandas as pd
-import numpy as np
 import dask.dataframe as dd
-
-from pathlib import Path
-from sklearn.preprocessing import PowerTransformer
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
-from sklearn.impute import SimpleImputer
-
 import config as config
 
+from pathlib import Path
 
-data_path = config.PATH_WORKING_DIR / 'datasetv10*.csv'
+
+working_dir = config.PATH_WORKING_DIR
+working_dir.mkdir(exist_ok=True)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(levelname)s] %(asctime)s - %(name)s - %(funcName)s: l%(lineno)d: %(message)s',
+    handlers=[
+        logging.FileHandler(working_dir / Path('raw_loans_data.log')),
+        logging.StreamHandler()
+    ]
+)
+logger = logging.getLogger(__name__)
+
+data_path = working_dir / 'datasetv12_*.csv'
 dtypes = {
-    'AGRI_gross_farming_inc': 'float64',
-    'AGRI_net_farm_inc': 'float64',
-    'ALL_cp_ltd': 'float64',
-    'CORP_net_cash_income': 'float64',
-    'CORP_net_income_growth': 'float64',
+    'BRR': 'float64',
+    'defaults_3_months': 'float64',
+    'defaults_6_months': 'float64',
+    'defaults_9_months': 'float64',
+    'defaults_12_months': 'float64',
+    'dunning_level_code': 'float64',
+    'impaired': 'float64',
+    'naics_id': 'float64',
+    'days_in_arrears': 'float64',
+    'mth_since_brr_update': 'float64',
+    'transaction_type_Customer_Transfer': 'float64',
+    'transaction_type_Incoming_Wire': 'float64',
+    'transaction_type_Outgoing_Wire': 'float64',
+    'BEACON': 'object',    # 'X' in the column
     'dunning_level': 'object',
-    'syndicated_loan': 'object',
-    'BEACON': 'object',
+    'syndicated_loan': 'object'
 }
 ddf = dd.read_csv(data_path, dtype=dtypes)
 data = ddf.compute()    # compile to pd.DataFrame
+logger.info('read csv files')
+
+data.drop(columns=['BEACON', 'dunning_level'], inplace=True)
+logger.info(f'drop columns with 90% missing values')
 
 start_date = '2013-01-31'
-end_date = '2021-08-31'
-
-data['BEACON'] = pd.to_numeric(data['BEACON'], errors='coerce')    # 'X' in the column
+end_date = '2021-12-31'
 data['cal_day'] = pd.to_datetime(data['cal_day'], errors='coerce')
-data = data[data['cal_day'].notnull()].copy()
-data = data[(data['cal_day'] >= start_date) & (data['cal_day'] <= end_date)].copy()
-loans_data = data[data['has_loan'] == 1]
-loans_data = loans_data.sort_values('cal_day')
-loans_data = loans_data.reset_index(drop=True)
+data = data[data['cal_day'].notnull()]
+data = data[(data['cal_day'] >= start_date) & (data['cal_day'] <= end_date)]
+logger.info(f'select data from {start_date} to {end_date}')
 
-config.PATH_WORKING_DIR.mkdir(exist_ok=True)
+loans_data = data[data['has_loan'] == 1]
+logger.info('get observations having loan')
+
+loans_data = loans_data.sort_values('cal_day').reset_index(drop=True)
 loans_data.to_parquet(config.PATH_RAW_DATA)
+logger.info('save raw data')
