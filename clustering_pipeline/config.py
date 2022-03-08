191baad777ac1dc4608f0d7de4ddceeca95ffd0d
wswordform <25746440+chaosWsF@users.noreply.github.com>
2022-03-07 19:47:14 -0700

    add data preprocess

--- clustering_pipeline/config.py
+++ clustering_pipeline/config.py
@@ -1,14 +1,13 @@
 from pathlib import Path
-from scipy.stats import uniform
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
 
 
 # paths
 PATH_WORKING_DIR = Path('working_dir')
 PATH_RAW_DATA = PATH_WORKING_DIR / Path('loans_data.pq')
 
-# Feature Engineering
+# features
 NON_FEATURE_COLUMNS = [
     'cal_day',
     'kmeans_label',
@@ -24,6 +23,8 @@ NON_FEATURE_COLUMNS = [
 
 CATEGORICAL_FEATURES = ['trend_code', 'dunning_level', 'syndicated_loan']
 
+COLUMN_TO_IMPUTE_MEDIAN = ['days_in_arrears', 'dunning_level_code']
+
 ID_COLUMNS = ['cal_day', 'bus_ptnr_group']
 
 BASE_LABEL_COLUMN = 'impaired'
@@ -34,46 +35,21 @@ EXPERIMENT_NAME = 'train_with_pipelines'
 
 INITIAL_TRAIN_PERIOD = ('2013-03-31', '2015-03-01')
 
-# train and test periods
 TRAIN_PERIOD = ('2013-04-01', '2018-12-31')
 TEST_PERIOD = ('2019-04-01', '2019-12-31')
 
-TIME_LAGS = [1, 3, 6, 12]
-
 # Models
-DECISION_TREE_PARAMS = {
-    'max_depth': range(2, 3),
-    'criterion': ['entropy', 'gini'],
-    'class_weight': ['balanced']
+pca = {
+    'ESTIMATOR': PCA(),
+    'PARAMS': {
+        'n_components': 0.95
+    }
 }
 
-DECISION_TREE = {
-    'ESTIMATOR': DecisionTreeClassifier(),
-    'PARAMS': DECISION_TREE_PARAMS
+kmeans = {
+    'ESTIMATOR': KMeans(),
+    'PARAMS': {
+        'n_clusters': 10
+    }
 }
 
-RANDOM_FOREST_PARAMS = {
-    'criterion': ['entropy', 'gini'],
-    'bootstrap': [False],
-    'n_estimators': [200],
-    'max_depth': range(8, 15),
-    # 'min_samples_split': [200, 500, 800, 1000],
-    # 'min_samples_leaf': Random(Discrete([100, 200, 400, 700]))
-    'max_features': uniform(0.1, 0.4),
-    #     'min_weight_fraction_leaf': uniform(0, 0.05),
-    'n_jobs': [-1],
-    'class_weight': ['balanced'],
-    'verbose': [1]
-}
-RANDOM_FOREST = {
-    'ESTIMATOR': RandomForestClassifier(),
-    'PARAMS': RANDOM_FOREST_PARAMS
-}
-
-MODELS = [('decision_tree', DECISION_TREE)]
-
-# CV
-NUM_INNER_SPLITS = 5
-NUM_OUTER_SPLITS = 5
-NUM_ITER = 1
-CV_SCORE = 'f1_macro'
---
+++ clustering_pipeline/data_preprocessing.py
@@ -0,0 +1,62 @@
+import logging
+import config as config
+import numpy as np
+# import pandas as pd
+# import category_encoders as ce
+
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import PowerTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class DataPreprocessor:
+    def __init__(self) -> None:
+        self.logger = logging.getLogger(__name__)
+        self.non_feat = config.NON_FEATURE_COLUMNS
+        self.cat_feat = config.CATEGORICAL_FEATURES
+    
+    def preprocess(self, data):
+        """
+        data: pd.DataFrame
+        """
+        logger = self.logger
+        
+        cols = data.columns.difference(self.non_feat).to_list()
+        cat_cols = list(set(cols).intersection(self.cat_feat))
+        num_cols = list(set(cols).difference(cat_cols))
+        logger.info(f'{len(num_cols)} numeric, {len(cat_cols)} categorical')
+
+        col_median = config.COLUMN_TO_IMPUTE_MEDIAN
+        col_const = list(set(num_cols).difference(col_median))
+
+        ppl_median = Pipeline([
+            ('median', SimpleImputer(strategy='median')),
+            ('power', PowerTransformer(method='yeo-johnson', standardize=True))
+        ])
+        ppl_const = Pipeline([
+            ('constant', SimpleImputer(strategy='constant', fill_value=0.0)),
+            ('power', PowerTransformer(method='yeo-johnson', standardize=True))
+        ])
+        transformer = ColumnTransformer(transformers=[
+            ('ne1', ppl_median, col_median),
+            ('ne2', ppl_const, col_const),
+            ('ce', catEncoder(), cat_cols)
+            # ('ce', ce.OneHotEncoder(), cat_cols)
+        ], remainder='drop', n_jobs=-1)
+
+        cleaned_data = transformer.fit_transform(data)
+        feat_cols = col_median + col_const + cat_cols
+        logger.info('apply mixed strategy for missing values')
+
+        return cleaned_data, feat_cols
+
+
+class catEncoder(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        return np.array([[] for _ in range(len(X))])
+
